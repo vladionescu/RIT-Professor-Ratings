52ce00fea2a762b7809c435cb7e95f25d137cfdc
Vlad Ionescu <vlad@vladionescu.com>
2014-11-17 00:50:36 -0800

    Initial commit, v1 is a lie, this doesn't do anything at the moment. BLOCKING on lack of API.

---
+++ README.md
@@ -0,0 +1,20 @@
+RIT Professor Ratings
+=====================
+
+I'm trying to solve an efficiency problem that arises when planning classes for enrollment. To cut down on the amount of time navigating back and forth between classsearch.rit.edu and ratemyprofessors.com I will provide each Professor's stats under their name in the Class Search results. This will probably be visible on-click of a little arrow next to the Prof's name, or something of the sort.
+
+This functionality will exist in a Chrome Extension, and it might get ported to other browsers in the future, when I've had more sleep.
+
+Status
+------
+Currently BLOCKING on RateMyProfessors lacking a public API. A message was sent to the site overlords today (Nov 17 2015) and a response is not on my list of things to expect this Christmas. Will probably have to resort to page scraping. Hooray.
+
+Feature Requests/Bugs
+---------------------
+Fork & send a Pull Request, or feed me coffee until this does what you desire.
+
+License
+-------
+Do whatever you want!
+
+Alternatively, if you feel so inclined, I accept donations in the form of hugs, good coffee, and compliments.
---
+++ contentscript.js
@@ -0,0 +1,61 @@
+/*
+* Copyright (c) 2014 Vlad Ionescu <github.com/vladionescu OR @uscenoi>
+* Use of this source code is governed by a do what you want license.
+*/
+
+/**
+ * Performs an XMLHttpRequest to RateMyProfessors' API to get a Prof's statistics,
+ *   such as hotness, easiness, and so on.
+ *
+ * Accepts a Prof's 'First Last' name as the first arg.
+ * The second arg is a hollaback, and the third is the arg to the hollaback.
+ */
+function getProfStats(name, callback, el) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function(data) {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        var data = JSON.parse(xhr.responseText);
+        callback(data, el);
+      } else {
+        callback(null, null);
+      }
+    }
+  }
+
+  // Note: any URLs must be matched by a permission in manifest.json
+  //
+  // FIXME: No public API. Poked RMP 11/17/2015 using Contact Us form about using an API. Not expecting a reply.
+  var url = 'http://www.ratemyprofessors.com/api/vNONEXISTENT/'+name;
+  xhr.open('GET', url, true);
+  xhr.send();
+};
+
+/**
+ * Parses stats response and appends them to the specified element.
+ *
+ * First arg is the response from querying RMP.
+ * Second arg is the DOM element to append the stats to.
+ */
+function onText(data, el) {
+  // Only continue if we've actually got something.
+  if (data) {
+    var stats_el = document.createElement('div');
+
+    console.log('cuckoo! cuckoo!')
+  
+    // Shamelessly stolen from sample. This is a nice way to handle CSS.
+    stats_el.style.cssText = [
+      'background-color: #ffd700;',
+      'background-image: -webkit-repeating-linear-gradient(' +
+        '45deg, transparent, transparent 35px,' +
+        'rgba(0,0,0,.1) 35px, rgba(0,0,0,.1) 70px);',
+      'color: #000;',
+      'padding: 10px;',
+      'font: 14px Arial;'
+    ].join(' ');
+    document.body.parentElement.insertBefore(stats_el, el);
+  }
+};
+
+getProfStats('Bob Ross', onText, document.getElementById('bobross'));
